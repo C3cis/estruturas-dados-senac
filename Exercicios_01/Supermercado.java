20cf0648deca9b287645dc305831e532f00c4387
Cecilia Russano <cecistudy23@gmail.com>
2026-04-14 20:29:26 -0300

    correção do codigo

--- Exercicios_01/Supermercado.java
+++ Exercicios_01/Supermercado.java
@@ -3,41 +3,43 @@ package Exercicios_01;
 import java.util.Scanner;
 
 public class Supermercado {
+
     public static void main(String[] args) {
-        
-    double[] faturamentos = new double[5];
-    int valor = 0;
-
-    leitura(faturamentos, valor);
-}
-public static double leitura(double[] faturamentos, int valor){
-    Scanner scan = new Scanner(System.in);
-    for (int i = 0; i <= faturamentos.length; i++){
-        System.out.print("Coloque o faturamento do dia");
-        faturamentos[i] = scan.nextDouble(); 
-        valor++;
-        System.out.println("faturamentos " + faturamentos[i] );
-    } return valor;
-}
-    public static int caixasIndi(int menorValor, int maiorValor, int valor){
-            int caixas = new int[5];
-        
+        double[] faturamentos = new double[5];
+        leituradosFaturamentos(faturamentos);   
+        calculosdosIndices(faturamentos);    
     }
-    public static int parametros(int valor)
-    int soma = 0;
-    int maiorValor = 0;
-    int menorValor = 0;
-    int indiceMaior = 0;
-    int indiceMenor = 0;
-    if(maiorValor > faturamento.length){
-    return maiorValor;
-}if (menorValor < faturamentos.length){
-    return menorValor;
-}if (indiceMaior > valor.length){
-    return indiceMaior;
-}if (indiceMenor < valor.length){
-    return indiceMenor
-        } 
-    
 
+    public static void leituradosFaturamentos(double[] faturamentos) {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < faturamentos.length; i++) {  
+            System.out.print("Faturamento do caixa " + (i + 1) + ": R$"); 
+            faturamentos[i] = scan.nextDouble();
+        }
+    }
+    public static void calculosdosIndices(double[] faturamentos) {
+        double soma       = 0;
+        double maiorValor = faturamentos[0]; 
+        double menorValor = faturamentos[0];
+        int    indiceMaior = 0;
+        int    indiceMenor = 0;
+
+        for (int i = 0; i < faturamentos.length; i++) {
+            soma += faturamentos[i];
+
+            if (faturamentos[i] > maiorValor) {  
+                maiorValor = faturamentos[i];
+                indiceMaior = i;
+            }
+            if (faturamentos[i] < menorValor) {
+                menorValor = faturamentos[i];
+                indiceMenor = i;
+            }
+        }
+
+        System.out.println("Total do dia: R$" + soma);
+        System.out.println("Media por caixa: R$" +  (soma / 5));
+        System.out.printf("Maior venda: Caixa %d com R$%.2f%n", (indiceMaior + 1), maiorValor);
+        System.out.printf("Menor venda: Caixa %d com R$20%.2f%n", (indiceMenor + 1), menorValor);
+    }
 }
